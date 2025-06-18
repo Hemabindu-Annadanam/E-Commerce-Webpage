45150d49e70c54b2d3ad4de1045e91f07bf40819
Hemabindu-Annadanam <hemabindu.annadanam@fissionlabs.com>
2025-06-18 14:44:25 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,19 +4,19 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux';
 import store from './Store.tsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
+import {  HashRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
-const basename = import.meta.env.PROD ? '/E-Commerce-Webpage/' : '/';
+// const basename = import.meta.env.PROD ? '/E-Commerce-Webpage/' : '/';
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
-  root.render(
-   <Provider store={store}>
-    <BrowserRouter basename={basename}>
+ root.render(
+  <Provider store={store}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
-  );
+);
 } else {
   throw new Error("Root element not found");
 }
